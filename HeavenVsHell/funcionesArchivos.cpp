c6f7c2dbe9fba3d2922e9c0e6af30afa12d7b1d1
Brayan Marín Quirós <brayanmq11288@gmail.com>
2020-08-01 19:33:44 -0600

    Se agregó la función de Herencia en la función "sumarAcciones"

--- HeavenVsHell/funcionesArchivos.cpp
+++ HeavenVsHell/funcionesArchivos.cpp
@@ -35,19 +35,55 @@ void funcionesArchivos::sumarAcciones(ListaDoble_Personas * pListaPersonas, bool
     if (pAccionBuena) {// Si pAccionBuena es true agrega buenas acciones
 
         while (tmp != NULL) {
+
             for (int i = 0; i < 7; i++) {
                     tmp->buenasAcciones[i] += random(0, 100);
             }
+
             tmp = tmp->next;
+
         }
 
     } else { // Si pAccionBuena es false agrega pecados
 
+        int numHeradado = 0;
+         int numAleatorio = 0;
+
         while (tmp != NULL) {
+
             for (int i = 0; i < 7; i++) {
-                    tmp->pecados[i] += random(0, 100);
+
+                    numAleatorio = random(0, 100); // Calcula el número aleatorio para asignar en los pecados
+                    tmp->pecados[i] += numAleatorio;
+
+                    //Empieza la herencia
+                    if (!tmp->listaHijos.empty()){ //Si la lista de hijos del padre no está vacía
+
+                        numHeradado = numAleatorio * 0.5; //Obtiene el 50% de los pecados
+
+                        for(Persona * hijo : tmp->listaHijos){ //For que recorre a los hijos
+
+                            hijo->pecados[i] += numHeradado; // Asigna el 50% de pecados del padre
+
+                            if(!hijo->listaHijos.empty()){ //Si la lista de nietos del padre no está vacía
+
+                                numHeradado = numAleatorio * 0.25; //Obtiene el 25% de los pecados
+
+                                for(Persona * nieto : hijo->listaHijos){ //For que recorre a los nietos
+
+                                    nieto->pecados[i] += numHeradado; // Asigna el 25% de pecados del padre
+
+                                }
+
+                            }
+
+                        }
+                    }
+
             }
+
             tmp = tmp->next;
+
         }
     }
 }
